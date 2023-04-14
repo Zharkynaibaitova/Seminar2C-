d7184ad6343f93355a333af35e52298d34c1397c
Zharkyn <zharkynaibaitova@gmail.com>
2023-04-14 22:11:26 +0600

    kratno dvum chislam

---
+++ Zadacha14kratnoOdnovremenno/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверякет, кртано ли оно одновременно 7 и 23
+
+Console.WriteLine ("Введите 1 число");
+int num =Convert.ToInt32 (Console.ReadLine());
+
+if (num % 7 ==0 &&  num % 23 == 0)
+{
+    Console.WriteLine ($"{num} -> да");
+}
+else
+{
+    Console.WriteLine ($"{num} -> нет");
+}
