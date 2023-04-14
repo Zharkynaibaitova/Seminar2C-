0d4d76ee519fcb92e4917b053ee5bb7f178a4b68
Zharkyn <zharkynaibaitova@gmail.com>
2023-04-14 22:24:57 +0600

    является ли число квадратом другогою

---
+++ Zadacha16Kvadrat/Program.cs
@@ -0,0 +1,18 @@
+﻿// напишите програииу, которая принимает на вход два числа и проверяет, является ли число квадратом другогою
+// 5б 25 -> да
+// -4, 16 -> да
+// 8, 9 -> нет
+
+Console.WriteLine ("Введите 1 число");
+int num1 =Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите 2 число");
+int num2 =Convert.ToInt32 (Console.ReadLine());
+if (num1 == num2 * num2 || num2 == num1 * num1)
+{
+    Console.WriteLine ($"{num1}, {num2} -> да");
+}
+else
+{
+    Console.WriteLine ($"{num1}, {num2} -> нет");
+}
+
